282e1b5b028e0722ebe7bfff19b555ae896e8c36
tswbyaj <tswbyaj@gmail.com>
2013-12-19 10:45:53 -0800

    initial blog commit

---
+++ kaneroad/blog/admin.py
@@ -0,0 +1,20 @@
+
+from blog.models import *
+from django.contrib import admin
+
+
+
+### Admin
+
+
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ["title"]
+    display_fields = ["title", "created"]
+
+class CommentAdmin(admin.ModelAdmin):
+    display_fields = ["post", "author", "created"]
+
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
